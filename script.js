742c6cb077a12682a656507e124d54104a6902a2
AntonOshurek <oshurekweb@gmail.com>
2021-07-19 17:49:46 +0200

    adding the ability to remove todo items

--- script.js
+++ script.js
@@ -27,7 +27,6 @@ showPage();
 
 //TODO logick
 
-
 const inputAddMessege = document.querySelector('#input__text');
 const inputAddDate = document.querySelector('#input__date');
 const btnAddMessege = document.querySelector('.main__form--button');
@@ -48,55 +47,78 @@ btnAddMessege.addEventListener('click', createTodoElem);
 
 function createTodoElem (e) {
     e.preventDefault();
-    let newTodo = {  //objeckt for new todo messege
-        todo: inputAddMessege.value,
-        deadline: inputAddDate.value,
-    };
 
-    inputAddMessege.value = "";  //clear input text
-    inputAddDate.value = "";    //clear input date
+    if(!inputAddMessege.value) {
+        inputAddMessege.placeholder = "Вы не ввели данные!";
 
-    todoList.push(newTodo); //push new objeckt to "todo" array
-    displayMesseges();  //create new todo list elem for page
+        inputAddMessege.classList.remove('input__error');
+        inputAddMessege.offsetWidth = inputAddMessege.offsetWidth;
+        inputAddMessege.classList.add('input__error');  
 
-    localStorage.setItem('todo', JSON.stringify(todoList)); //передали в локалсторейдж
+    } else {
+        inputAddMessege.classList.remove('input__error');
+        inputAddMessege.placeholder = "данные приняты";
+
+        let newTodo = {  //objeckt for new todo messege
+            todo: inputAddMessege.value,
+            deadline: inputAddDate.value,
+        };
+    
+        inputAddMessege.value = "";  //clear input text
+        inputAddDate.value = "";    //clear input date
+    
+        todoList.push(newTodo); //push new objeckt to "todo" array
+        displayMesseges();  //create new todo list elem for page
+    
+        localStorage.setItem('todo', JSON.stringify(todoList)); //передали в локалсторейдж
+    }
+
 }
 
 function displayMesseges() {
     let displayMessege = '';
+
     todoList.forEach(function(item, i) {
         displayMessege += `
-        <li class="main__todo__list--elem">
-        <div class="main__todo__list--elem--text">${item.todo}</div>  
-        <div class="todo__list--elem--setings">
-             <button class="btn todo__list--elem--setings--btn delite-btn" data-delite-btn="${i}" type="button">Delite</button>
-             <button class="btn todo__list--elem--setings--btn done-btn" data-done-btn="${i}" type="button">is done</button>
-        </div>
-        <div class="main__todo__list--elem--time">
-            <span class="days date__span">05 : days</span>
-            <span class="hourse date__span">16 : hourse</span>
-            <span class="minutes date__span">45 : minutes</span>
-            <span class="seconds date__span">33 : seconds</span>
-        </div>
-    </li>
-        `
+            <li class="main__todo__list--elem">
+            <div class="main__todo__list--elem--text">${item.todo}</div>  
+            <div class="todo__list--elem--setings">
+                <button class="btn todo__list--elem--setings--btn delite-btn" data-delite-btn="${i}" type="button">Delite</button>
+                <button class="btn todo__list--elem--setings--btn done-btn" data-done-btn="${i}" type="button">is done</button>
+            </div>
+            <div class="main__todo__list--elem--time">
+                <span class="days date__span">05 : days</span>
+                <span class="hourse date__span">16 : hourse</span>
+                <span class="minutes date__span">45 : minutes</span>
+                <span class="seconds date__span">33 : seconds</span>
+            </div>
+            </li>
+        `;
         mainTodoList.innerHTML = displayMessege;
     });
+
+    deliteListElem();
 };
 
-//delete list elem
+function deliteListElem() {
+    const deliteBtn = document.querySelectorAll('.delite-btn');
 
-const deliteBtn = document.querySelectorAll('.delite-btn');
+    deliteBtn.forEach(btn => {
+        btn.addEventListener('click', deleteElem);
+    });
 
-deliteBtn.forEach(btn => {
-    btn.addEventListener('click', deleteElem);
-});
+    function deleteElem(e) {
+        let btnIndex = e.target.getAttribute('data-delite-btn');
+        todoList.splice(btnIndex, 1);
+        
+        localStorage.setItem('todo', JSON.stringify(todoList));     
 
-function deleteElem(e) {
-    let btnIndex = e.target.getAttribute('data-delite-btn');
-    todoList.splice(btnIndex, 1);
-    localStorage.setItem('todo', JSON.stringify(todoList));
-    console.log(localStorage);
+        if(todoList.length <= 0) {
+            mainTodoList.innerHTML = 'Add new list elem';
+        } else {
+            displayMesseges();
+        }   
+    };
 };
 
 
