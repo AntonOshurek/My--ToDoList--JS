3e36ca27b986267c5f8ca93ab8338c6f741219d2
AntonOshurek <oshurekweb@gmail.com>
2021-07-21 21:05:32 +0200

    add a timer js logick

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ function createTodoElem (e) {
     e.preventDefault();
 
     if(!inputAddMessege.value) {
-        inputAddMessege.placeholder = "Вы не ввели данные!";
+        inputAddMessege.placeholder = "you have not entered data!";
 
         inputAddMessege.classList.remove('input__error');
         inputAddMessege.offsetWidth = inputAddMessege.offsetWidth;
@@ -57,7 +57,7 @@ function createTodoElem (e) {
 
     } else {
         inputAddMessege.classList.remove('input__error');
-        inputAddMessege.placeholder = "данные приняты";
+        inputAddMessege.placeholder = "data received";
 
         let newTodo = {  //objeckt for new todo messege
             todo: inputAddMessege.value,
@@ -79,6 +79,7 @@ function displayMesseges() {
     let displayMessege = '';
 
     todoList.forEach(function(item, i) {
+
         displayMessege += `
             <li class="main__todo__list--elem">
             <div class="main__todo__list--elem--text">${item.todo}</div>  
@@ -86,15 +87,18 @@ function displayMesseges() {
                 <button class="btn todo__list--elem--setings--btn delite-btn" data-delite-btn="${i}" type="button">Delite</button>
                 <button class="btn todo__list--elem--setings--btn done-btn" data-done-btn="${i}" type="button">is done</button>
             </div>
-            <div class="main__todo__list--elem--time">
-                <span class="days date__span">05 : days</span>
-                <span class="hourse date__span">16 : hourse</span>
-                <span class="minutes date__span">45 : minutes</span>
-                <span class="seconds date__span">33 : seconds</span>
+            <div class="main__todo__list--elem--timer">
+                <p class="date__span"><span id="days__${i}">00</span> : days</p>
+                <p class="date__span"><span id="hourse__${i}">00</span> : hourse</p>
+                <p class="date__span"><span id="minutes__${i}">00</span> : minutes</p>
+                <p class="date__span"><span id="seconds__${i}">00</span> : seconds</p>
             </div>
             </li>
         `;
+
         mainTodoList.innerHTML = displayMessege;
+        console.log(todoList);
+        setClock(i, item.deadline);
     });
 
     deliteListElem();
@@ -120,6 +124,57 @@ function deliteListElem() {
         }   
     };
 };
+
+//timer todo logick
+
+function gettime(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date());
+    const days = Math.floor(t / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((t / (1000 * 60 * 60) % 24));  //получаем остаток от деления (остаток от 24 часов, что бы не выводилось больше часов чем есть в сутках)
+    const minutes = Math.floor((t / 1000 / 60) % 60);
+    const seconds = Math.floor((t / 1000) % 60);
+
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+};
+
+function getZero (num) {  //функция для добавления нуля 
+    if (num >= 0 && num < 10) {
+        return `0${num}`;
+    } else {
+        return num;
+    }
+}
+
+function setClock(selector, endtime) {
+    const days = document.querySelector(`#days__${selector}`);
+    const hours = document.querySelector(`#hourse__${selector}`);
+    const minutes = document.querySelector(`#minutes__${selector}`);
+    const seconds = document.querySelector(`#seconds__${selector}`);
+    const timeInterval = setInterval(updateClock, 1000);
+
+    updateClock(); //для устранения мигания таймера запускаем функцию один раз при загрузке страницы
+
+    function updateClock () {
+        const t = gettime(endtime);
+        //console.log(t);
+
+        days.innerHTML = getZero(t.days);
+        hours.innerHTML = getZero(t.hours);
+        minutes.innerHTML = getZero(t.minutes);
+        seconds.innerHTML = getZero(t.seconds);
+
+        if (t.total <= 0) {
+            clearInterval(timeInterval);
+        }
+    };
+};
+
 
 
 
